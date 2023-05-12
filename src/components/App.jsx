a75cdb1f77baf4b687ca5ec7c42b17486aefb34c
SergiiMal <ser4ester@gmail.com>
2023-05-12 23:05:45 +0300

    +

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import ContactForm from './ContactForm/ContactForm';
+import ContactsList from './ContactsList/ContactsList';
+import Filter from './Filter/Filter';
+
+import { Container, Title, TitleContacts } from './App.styled';
+
+function App() {
+  return (
+    <Container>
+      <Title>PhoneBook</Title>
+      <ContactForm />
+      <TitleContacts>Contacts</TitleContacts>
+      <Filter />
+      <ContactsList />
+    </Container>
+  );
+}
+
+export default App;
